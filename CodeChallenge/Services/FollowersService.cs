b215ec577769c145d5d478f50d5851db4fbea739
anvortmeier <anvortmeier@users.noreply.github.com>
2018-11-06 19:24:05 -0600

    clean up followerService

--- CodeChallenge/Services/FollowersService.cs
+++ CodeChallenge/Services/FollowersService.cs
@@ -7,8 +7,7 @@ namespace CodeChallenge.Services
 	public class FollowersService
 	{
 		private readonly CodeChallengeService client;
-		private readonly int DEFAULT_DEPTH = 3;
-		private string githubId;
+		private readonly string githubId;
 
 		public FollowersService(string githubId)
 		{
@@ -21,43 +20,19 @@ namespace CodeChallenge.Services
 			var followers = LimitFollowersToFive(githubId);
 			foreach(var followerA in followers)
 			{
-				string githubIdA = followerA.id.ToString();
-				followerA.followers = LimitFollowersToFive(githubIdA).ToList();
+				followerA.followers = LimitFollowersToFive(followerA.id.ToString()).ToList();
 				foreach(var followerB in followerA.followers)
 				{
-					string githubIdB = followerB.id.ToString();
-					followerB.followers = LimitFollowersToFive(githubIdB).ToList();
-				}
-				
-			}
-
-			return followers;
-		}
-
-		public IEnumerable<RootObject> ViewFollowers(string id)
-		{
-			return ViewFollowers(id, DEFAULT_DEPTH);
-		}
-
-		public IEnumerable<RootObject> ViewFollowers(string id, int depth)
-		{
-			var followers = LimitFollowersToFive(id).ToList();
-			if (depth == 0) { return followers; }
-			else
-			{
-				foreach (var follower in followers)
-				{
-					string githubId = follower.id.ToString();
-					ViewFollowers(githubId, depth - 1);
+					followerB.followers = LimitFollowersToFive(followerB.id.ToString()).ToList();
 				}
 			}
+
 			return followers;
 		}
 
 		private IEnumerable<RootObject> LimitFollowersToFive(string githubID )
 		{
-			//return client.GetGithubIdFollowers(githubId).Take(5);
-			return client.GetGithubIdFollowers(githubId).Take(2);
+			return client.GetGithubIdFollowers(githubId).Take(5);
 		}
 	}
 }
